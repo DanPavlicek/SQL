cb462958941c53e0f063eda33c623b31874314fb
Dan Pavlíček <dan.pavlicek@seznam.cz>
2025-01-18 18:47:57 +0100

    Pridani otazek 1,2,3 v separatnich souborech

---
+++ Engeto projekt otazka cislo dva.sql
@@ -0,0 +1,121 @@
+-- Otázka č.2: Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+
+-- 1. Provedení selectů v tabulkách czechia_price, czechia_price_category, czechia_region
+
+SELECT *
+FROM czechia_price AS cp 
+
+SELECT *
+FROM czechia_price_category AS cpc 
+
+SELECT *
+FROM czechia_region AS cr 
+
+-- 2. připojení sloupců z czechia price category a czechia region do czechia_price pro lepší přehled a porozumění a vytvoření view mezikrok_ceny
+
+CREATE OR REPLACE VIEW mezikrok_ceny AS
+SELECT 	cp.*, 
+		cpc.name AS kategorie, 
+		cpc.price_value AS velikostbaleni, 
+		cpc.price_unit AS jednotka, 
+		cr.name AS kraj
+FROM 
+		czechia_price AS cp 
+LEFT JOIN 
+		czechia_price_category cpc ON cpc.code = cp.category_code 
+LEFT JOIN 
+		czechia_region cr ON cr.code = cp.region_code 
+
+-- 3. identifikace sloupců s null hodnotami - existují řádky s null hodnotami ve sloupci region_code - kraj, pro otázku č. 2 nepodstatné, podle mě
+
+SELECT *
+FROM 
+		mezikrok_ceny AS mc 
+WHERE 	
+		mc.id IS NULL OR 
+		mc.value IS NULL OR 
+		mc.category_code IS NULL OR 
+		mc.date_from IS NULL OR 
+		mc.date_to IS NULL OR 
+		mc.region_code IS NULL
+
+
+-- 4. Identifikace category code mléko a chleba 
+
+SELECT DISTINCT 
+		mc.kategorie, mc.category_code 
+FROM 
+		mezikrok_ceny AS mc 
+ORDER BY 
+		mc.kategorie ASC
+
+-- Výsledek mléko (114201) a chleba (111301)
+
+-- 5.  zúžení výběru na mléko a chleba a selecty zaměřžeené na datumy - první období 2006 a poslední 2018, czechia payroll má rozmezí 2000 až 2021
+
+SELECT 
+		*, year (date_from)
+FROM mezikrok_ceny AS mc 
+WHERE 
+		mc.category_code = 114201 or 
+		mc.category_code = 111301
+ORDER BY 
+		mc.date_from DESC 
+
+-- 6. Níže select pro zobrazení průměrných cen za rok 2006 a 2018 
+
+SELECT
+    mc.category_code, 
+    mc.kategorie, 
+    YEAR(DATE(mc.date_from)) AS year_from, 
+    AVG(mc.value) AS avg_value
+FROM 
+    mezikrok_ceny AS mc
+WHERE 
+    (mc.category_code = 111301 or mc.category_code = 114201)
+    AND YEAR(DATE(mc.date_from)) IN (2006, 2018)
+GROUP BY 
+    mc.category_code, 
+    mc.kategorie, 
+    YEAR(DATE(mc.date_from))
+ORDER BY 
+    mc.category_code, 
+    year_from;
+
+-- 7. Výsledkem jsou průměrné ceny:
+
+/*
+16.12364000000002 chleba 2006
+24.238500000000013 chleba 2018
+
+14.437840000000001 mleko 2006
+19.817555555555558 mleko 2018
+*/
+
+-- 8. Níže zjistím průměrné mzdy přes všechny odvětví za roky 2006 a 2018 a vypočítám, kolik si mohu koupit
+ 
+SELECT 
+		payroll_year, 
+		avg(value) AS prumerna_mzda,
+		CASE 
+        WHEN payroll_year = 2006 THEN (AVG(value) / 16.12364000000002)
+        WHEN payroll_year = 2018 THEN (AVG(value) / 24.238500000000013)
+        ELSE NULL
+    END AS pocet_ks_chleba,
+    	CASE
+    	WHEN payroll_year = 2006 THEN (AVG(value) / 14.437840000000001)
+    	WHEN payroll_year = 2018 THEN (AVG(value) / 19.817555555555558)
+    	ELSE NULL
+    END AS pocet_lt_mleka
+FROM 	mezikrok_mzdy AS mm 
+WHERE 
+		payroll_year IN (2006,2018)
+GROUP BY 
+		payroll_year  
+
+-- 9. Výsledkem je počet ks chleba a litrů mléka, které si za průměrnou mzdu ze všech odvětví můžeme koupit v letech 2006 a 2018
+/*
+payroll_year  prumerna_mzda    pocet_ks_chleba  pocet_lt_mleka
+2006	        21165.1842  	1312.68027632	  1465.95226229
+2018	        33091.4474	    1365.24320269	  1669.80469794
+*/
---
+++ Engeto projekt otazka cislo jedna.sql
@@ -0,0 +1,189 @@
+
+
+-- Otázka č.1 : Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+
+-- 1. Krok - obecné selecty pro všecny tabulky napojené na tabulku czechia_payroll cp dle ER Diagramů a studium výsledků
+
+SELECT *
+FROM czechia_payroll AS cp 
+
+SELECT *
+FROM czechia_payroll_calculation AS cpc 
+
+SELECT *
+FROM czechia_payroll_industry_branch AS cpib
+
+SELECT *
+FROM czechia_payroll_unit AS cpu 
+
+SELECT *
+FROM czechia_payroll_value_type AS cpvt 
+
+-- 2.Krok připojení druhých sloupců ze všech tabulcek k tabulce czechia_payroll s cílem porozumět všem údajům v tabulce czechia_payroll
+
+SELECT 
+		cp.*, 
+		cpc.name AS fyzneboprepoc, 
+		cpib.name AS odvetvi, 
+		cpu.name AS Kcneboosoby, 
+		cpvt.name AS prummzdaneboosoby
+FROM 
+		czechia_payroll cp 
+LEFT JOIN 
+		czechia_payroll_calculation cpc ON cp.calculation_code = cpc.code 
+LEFT JOIN 
+		czechia_payroll_industry_branch cpib ON cp.industry_branch_code  = cpib.code 
+LEFT JOIN 
+		czechia_payroll_unit cpu ON cp.unit_code = cpu.code
+LEFT JOIN 
+		czechia_payroll_value_type cpvt ON cp.value_type_code = cpvt.code 
+		
+/*3. Krok dle studia předchozích selectů se zaměřím na průměrnou mzdu zaměstnance (value_type_code 5958) a přepočtený počet zaměstanců zohledňující částečné úvazky (calculation code 200)
+   Cílem je se zaměřit na průměrné mzdy a zohlednit částečné úvazky*/
+
+SELECT 
+		cp.*, 
+		cpc.name AS fyzneboprepoc, 
+		cpib.name AS odvetvi, 
+		cpu.name AS Kcneboosoby, 
+		cpvt.name AS prummzdaneboosoby
+FROM 
+		czechia_payroll AS cp 
+LEFT JOIN 
+		czechia_payroll_calculation cpc on cp.calculation_code = cpc.code 
+LEFT JOIN 
+		czechia_payroll_industry_branch cpib on cp.industry_branch_code  = cpib.code 
+LEFT JOIN 
+		czechia_payroll_unit cpu on cp.unit_code = cpu.code
+LEFT JOIN 
+		czechia_payroll_value_type cpvt on cp.value_type_code = cpvt.code
+WHERE 
+		value_type_code = 5958 
+		AND calculation_code = 200
+
+/*4. Krok zkontroluji výskyt null hodnot v selectu výše a to ve dvou krocích
+     a) vytvoření view: mezikrok_mzdy*/
+
+CREATE OR REPLACE VIEW mezikrok_mzdy AS
+SELECT 
+		cp.*, 
+		cpc.name AS fyzneboprepoc, 
+		cpib.name AS odvetvi, 
+		cpu.name AS Kcneboosoby, 
+		cpvt.name AS prummzdaneboosoby
+FROM 
+		czechia_payroll AS cp 
+LEFT JOIN 
+		czechia_payroll_calculation cpc ON cp.calculation_code = cpc.code 
+LEFT JOIN 
+		czechia_payroll_industry_branch cpib ON cp.industry_branch_code  = cpib.code 
+LEFT JOIN 
+		czechia_payroll_unit cpu ON cp.unit_code = cpu.code
+LEFT JOIN 
+		czechia_payroll_value_type cpvt ON cp.value_type_code = cpvt.code
+WHERE 
+		value_type_code = 5958 AND calculation_code = 200
+
+-- b) kontrola null hodnot - výsledek null hodnoty pouze ve sloupci industry branch code
+
+SELECT *
+FROM 
+		mezikrok_mzdy AS mm 
+WHERE 
+		mm.id IS NULL OR mm.value IS NULL OR mm.value_type_code
+IS NULL OR 
+		mm.unit_code IS NULL OR mm.calculation_code
+IS NULL OR 
+		mm.industry_branch_code IS NULL OR mm.payroll_year 
+IS NULL OR 
+		mm.payroll_quarter IS NULL
+
+-- 5. Krok aktualizace view o odstranění null hodnot z industry branch code a ověření
+
+CREATE OR REPLACE VIEW mezikrok_mzdy AS
+SELECT 
+		cp.*, 
+		cpc.name AS fyzneboprepoc, 
+		cpib.name AS odvetvi, 
+		cpu.name AS Kcneboosoby, 
+		cpvt.name AS prummzdaneboosoby
+FROM 
+		czechia_payroll AS cp 
+LEFT JOIN 
+		czechia_payroll_calculation cpc ON cp.calculation_code = cpc.code 
+LEFT JOIN 
+		czechia_payroll_industry_branch cpib ON cp.industry_branch_code  = cpib.code 
+LEFT JOIN 
+		czechia_payroll_unit cpu ON cp.unit_code = cpu.code
+LEFT JOIN 
+		czechia_payroll_value_type cpvt on cp.value_type_code = cpvt.code
+WHERE 
+		value_type_code = 5958 AND calculation_code = 200 AND industry_branch_code IS NOT NULL
+
+/* 6. Provádění dalších zkušebních selectů (uvádím jen relevatntní) např. rok 2021 má jen 2 kvartály a není vhodný pro zahrnutí do analýzy (souhnr po kompletních letech)
+   Aktualizuji view mezikrok.mzdy */
+
+SELECT *
+FROM 
+		mezikrok_mzdy AS mm
+WHERE 
+		mm.industry_branch_code = 'A'
+ORDER BY 
+		payroll_year DESC, 
+		payroll_quarter ASC
+
+-- 7. Aktualizace view níže, kdy vyjmu z  VIEW nekompletní rok 2021
+
+CREATE OR REPLACE VIEW mezikrok_mzdy AS
+SELECT 
+		cp.*, 
+		cpc.name AS fyzneboprepoc, 
+		cpib.name AS odvetvi, 
+		cpu.name AS Kcneboosoby, 
+		cpvt.name AS prummzdaneboosoby
+FROM 
+		czechia_payroll AS cp 
+LEFT JOIN 
+		czechia_payroll_calculation cpc on cp.calculation_code = cpc.code 
+LEFT JOIN 
+		czechia_payroll_industry_branch cpib on cp.industry_branch_code  = cpib.code 
+LEFT JOIN 
+		czechia_payroll_unit cpu on cp.unit_code = cpu.code
+LEFT JOIN 
+		czechia_payroll_value_type cpvt on cp.value_type_code = cpvt.code
+WHERE value_type_code = 5958 
+		AND calculation_code = 200 
+		AND industry_branch_code 
+		IS NOT NULL AND payroll_year != 2021
+
+-- 8. Sestavu seskupím po odvětví a letech, součtové pole value
+
+SELECT 
+		mm.odvetvi, 
+		mm.payroll_year, 
+		avg(value) AS prumernamzda
+FROM 
+		mezikrok_mzdy mm 
+GROUP BY 
+		mm.odvetvi, 
+		mm.payroll_year 
+
+/* 9. Rozhodnul jsem se zobrazit pouze roky 2000 a 2020 Prostým porovnáním vidím, že průměrné mdzy jsou v roce 2020 větší ve všechn odvětvích, než v roce 2000
+   Ostatní selecty přes všechny roky apod. mi přišly příliš nepřehledné */
+
+
+
+SELECT 
+		mm.odvetvi, 
+		mm.payroll_year, 
+		avg(value) AS prumernamzda
+FROM 
+		mezikrok_mzdy mm 
+WHERE 
+		payroll_year = 2000 
+		OR payroll_year = 2020
+GROUP BY 
+		mm.odvetvi, 
+		mm.payroll_year 
+
+-- 10. Odpověď na zadání: Průměrná mzda mezi roky 2000 a 2020 rostla ve všech odvětvích.
